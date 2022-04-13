db25b31b352d2b2823af852b6888ca775056b173
jose.rinaldodev <jose.rinaldodev@gmail.com>
2022-04-13 16:27:05 -0300

    controller Principal criada

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,21 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+| get post put delete patch options
 */
 
 Route::get('/', function () {
     return  'ola bem vindo ';
 });
+
+Route::get('/sobre-nos', function () {
+    return  'sobre nos';
+});
+
+Route::get('/contato', function () {
+    return  'contato';
+});
+
+
 
 
